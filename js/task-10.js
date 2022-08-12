f90fcba6be38ad9c8c628459ce3cd3994982ab9f
Senderion13 <serovandrey2004@gmail.com>
2022-08-12 15:33:41 +0300

    task_10

--- js/task-10.js
+++ js/task-10.js
@@ -1,4 +1,21 @@
+let boxes = document.querySelector("#boxes");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
+function createBoxes(amount) {
+  for (let index = 0; index < amount; index++) {
+    boxes.innerHTML += `<div style="background-color:${getRandomHexColor()};width:${30 + index * 10}px;height:${30 + index * 10}px"></div>`;
+  }
+}
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  createBoxes(document.querySelector("input[type=number]").value);
+})
+
+document.querySelector("[data-destroy]").addEventListener("click", () => {
+  boxes.innerHTML = "";
+})
